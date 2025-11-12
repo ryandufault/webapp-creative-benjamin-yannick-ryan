7a7cbde35856f588d6344ba606c2cf41c3588892
ryandufault <142917876+ryandufault@users.noreply.github.com>
2025-11-12 17:52:45 -0500

    + Avancement route / chapter 1

--- lesermentdelalune/src/views/HomeView.vue
+++ lesermentdelalune/src/views/HomeView.vue
@@ -1,11 +1,50 @@
 <template>
-    <h1>HomeView.vue</h1>
-    <AppHeader></AppHeader>
+  <div class="app-wrapper">
+    <h1>Le serment de la lune</h1>
+    <div class="btn-container">
+      <router-link class="btn" to="/chapter/1">Commencer</router-link>
+      <p class="btn">Continuer</p>
+      <p class="btn">Sauvegardes</p>
+      <p class="btn">Options</p>
+    </div>
+  </div>
 </template>
-  
+
 <script>
-import AppHeader from '../components/AppHeader.vue'
-  export default {
-    name: 'HomeView'
-  }
+export default {
+  name: 'HomeView'
+}
 </script>
+
+<style scoped>
+.app-wrapper {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+}
+
+h1 {
+  padding-top: 5vw;
+  text-align: center;
+}
+
+.btn-container {
+  padding-top: 5vw;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding-left: 15vw;
+}
+
+.btn {
+  color: black;
+  font-size: 1.2rem;
+  cursor: pointer;
+  padding: 10px 20px;
+  transition: color 0.3s;
+}
+
+.btn:hover {
+  color: rgb(139, 139, 139);
+}
+</style>
