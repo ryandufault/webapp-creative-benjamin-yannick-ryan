b775045efdc6fb0a3b0909c4224acb4caab68b3e
ryandufault <142917876+ryandufault@users.noreply.github.com>
2025-11-24 15:21:29 -0500

    + Components statindicator + statsbar

--- lesermentdelalune/src/components/StatIndicator.vue
+++ lesermentdelalune/src/components/StatIndicator.vue
@@ -1,52 +1,64 @@
 <template>
-    <div class="stat-indicator">
-      <p>Métrique</p>
-      <div class="stat-bar">
-        <div class="stat-fill"></div>
-      </div>
+  <div class="stat-indicator">
+    <div class="stat-bar">
+      <p>{{ name }}</p>
+      <!-- change de largeur selon la valeurr -->
+      <div class="stat-fill" :style="{ width: value + '%', backgroundColor: color }"></div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'StatIndicator',
-    data() {
-      return {
-        
-      }
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StatIndicator',
+  // received de statsbar avec le store pinia
+  props: {
+    name: {
+      type: String, // nom de la métrique
+    },
+    value: {
+      type: Number, // int
+    },
+    color: {
+      type: String, // parce que un hex c'est sous forme de string
     }
   }
-  </script>
-  
-  <style scoped>
-  .stat-indicator {
-    width: 200px;
-    margin: 10px 0;
-  }
-  
-  p {
-    color: black;
-    font-size: 1rem;
-    margin-left: 5px;
-    margin-bottom: 8px;
-  }
-  
-  .stat-bar {
-    width: 100%;
-    height: 20px;
-    background-color: rgba(0, 0, 0, 0.2);
-    border-radius: 2vw;
-    overflow: hidden;
-    position: relative;
-  }
-  
-  .stat-fill {
-    width: 50%; /* sera dynamique plus tard */
-    height: 100%;
-    background-color: red;
-    border-radius: 2vw;
-    position: relative;
-  }
-  
-  
-  </style>
+}
+</script>
+
+<style scoped>
+.stat-indicator {
+  width: 200px;
+  margin: 10px 0;
+  top: 0;
+}
+
+p {
+  position: absolute;
+  top: 50%;
+  left: 20%;
+  transform: translate(-50%, -50%);
+  color: #1F1B33;
+  font-size: 0.8vw;
+  font-family: Gothic;
+  margin: 0;
+  z-index: 4;
+}
+
+.stat-bar {
+  width: 100%;
+  height: 20px;
+  background-color: rgba(255, 255, 255, 0.15);
+  border-radius: 0.2vw;
+  overflow: hidden;
+  position: relative;
+}
+
+.stat-fill {
+  height: 100%;
+  border-radius: 0.2vw;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
