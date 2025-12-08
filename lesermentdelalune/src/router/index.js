2687362176e4204c2bb7133fb89b5bde2bd20214
YannickChamberland <142918103+YannickChamberland@users.noreply.github.com>
2025-12-08 14:38:01 -0500

    +nouvelle mise en ligne

--- lesermentdelalune/src/router/index.js
+++ lesermentdelalune/src/router/index.js
@@ -1,5 +1,5 @@
 /* On importe les méthodes nécessaires depuis le module `vue-router` */
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory  } from 'vue-router';
 
 /* 
 On importe les différentes views (pages) du projet, par exemple pour le 
@@ -30,9 +30,9 @@ const routes = [
 
 // Création du router via la méthode createRouter
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), // outil d'historique
-  routes // contient les routes déclarées préalablement
-});
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes
+})
 
 /*
 Le routeur étant maintenant configuré, nous allons l'exporter
